7aa97937410b382fce12e6b4a4f94047bc268cdc
EliteAsian <lavasnakegaming@gmail.com>
2020-08-27 18:20:20 -0400

    Initial commit

---
+++ VectorExtensions.cs
@@ -0,0 +1,125 @@
+﻿namespace UnityEngine {
+	public static class VectorExtensions {
+		/*
+		 *	Vector2 Utils
+		 */
+
+		public static Vector2 WithX(this Vector2 vec, float x) {
+			return new Vector2(x, vec.y);
+		}
+
+		public static Vector2 WithY(this Vector2 vec, float y) {
+			return new Vector2(vec.x, y);
+		}
+
+
+		public static Vector2 AddX(this Vector2 vec, float x) {
+			return new Vector2(vec.x + x, vec.y);
+		}
+
+		public static Vector2 AddY(this Vector2 vec, float y) {
+			return new Vector2(vec.x, vec.y + y);
+		}
+
+
+		public static Vector2 InvertX(this Vector2 vec) {
+			return new Vector2(-vec.x, vec.y);
+		}
+
+		public static Vector2 InvertY(this Vector2 vec) {
+			return new Vector2(vec.x, -vec.y);
+		}
+
+		public static Vector2 Invert(this Vector2 vec) {
+			return new Vector2(-vec.x, -vec.y);
+		}
+
+		/*
+		 *	Vector3 Utils
+		 */
+
+		public static Vector3 WithX(this Vector3 vec, float x) {
+			return new Vector3(x, vec.y, vec.z);
+		}
+
+		public static Vector3 WithY(this Vector3 vec, float y) {
+			return new Vector3(vec.x, y, vec.z);
+		}
+
+		public static Vector3 WithZ(this Vector3 vec, float z) {
+			return new Vector3(vec.x, vec.y, z);
+		}
+
+
+		public static Vector3 AddX(this Vector3 vec, float x) {
+			return new Vector3(vec.x + x, vec.y, vec.z);
+		}
+
+		public static Vector3 AddY(this Vector3 vec, float y) {
+			return new Vector3(vec.x, vec.y + y, vec.z);
+		}
+
+		public static Vector3 AddZ(this Vector3 vec, float z) {
+			return new Vector3(vec.x, vec.y, vec.z + z);
+		}
+
+
+		public static Vector3 InvertX(this Vector3 vec) {
+			return new Vector3(-vec.x, vec.y, vec.z);
+		}
+
+		public static Vector3 InvertY(this Vector3 vec) {
+			return new Vector3(vec.x, -vec.y, vec.z);
+		}
+
+		public static Vector3 InvertZ(this Vector3 vec) {
+			return new Vector3(vec.x, vec.y, -vec.z);
+		}
+
+		public static Vector3 Invert(this Vector3 vec) {
+			return new Vector3(-vec.x, -vec.y, -vec.z);
+		}
+
+		/*
+		 *  VectorInt -> Vector
+		 */
+
+		public static Vector3 ToVector3(this Vector2Int v) {
+			return new Vector3(v.x, v.y);
+		}
+
+		public static Vector3 ToVector3(this Vector3Int v) {
+			return new Vector3(v.x, v.y, v.z);
+		}
+
+
+		public static Vector2 ToVector2(this Vector2Int v) {
+			return new Vector2(v.x, v.y);
+		}
+
+		public static Vector2 ToVector2(this Vector3Int v) {
+			return new Vector2(v.x, v.y);
+		}
+
+		/*
+		 *  Vector -> VectorInt
+		 */
+
+		public static Vector3Int ToVector3Int(this Vector3 v) {
+			return new Vector3Int((int) v.x, (int) v.y, (int) v.z);
+		}
+
+		public static Vector3Int ToVector3Int(this Vector2 v) {
+			return new Vector3Int((int) v.x, (int) v.y, 0);
+		}
+
+
+		public static Vector2Int ToVector2Int(this Vector3 v) {
+			return new Vector2Int((int) v.x, (int) v.y);
+		}
+
+		public static Vector2Int ToVector2Int(this Vector2 v) {
+			return new Vector2Int((int) v.x, (int) v.y);
+		}
+	}
+}
---
+++ VectorExtensions.md
@@ -0,0 +1,4 @@
+# VectorExtensions
+This extension class adds additional functionality to the `Vector2` and `Vector3` classes.
+
+TODO.
